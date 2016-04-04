2a8612bb39421f5d32aa047992f6e8b0f074936e
jdmorales <jdmg44@hotmail.com>
2016-04-03 23:47:03 -0500

    step_four -> Imports HTML

---
+++ app.js
@@ -0,0 +1,75 @@
+function template(Name,Last_Name,Email){
+    var link = document.querySelector('link[rel="import"]').import;
+    console.log();
+
+    var template = link.querySelector('#profileTemplate').content;//document.querySelector('#profileTemplate').content;
+
+    template.querySelector('.name').textContent = Name;
+    template.querySelector('.last_name').textContent = Last_Name;
+    template.querySelector('.email').textContent = Email;
+
+    var clone = document.importNode(template, true); //Creates a copy of a node (node_template,deeps)
+
+    //document.body.appendChild(clone);
+    return clone;
+}
+
+function customElement(){
+    var userProfileProto = Object.create(HTMLElement.prototype);
+
+    userProfileProto.init = function(name,last_name,company) {
+        var element = template(name,last_name,company);
+        //this.appendChild(element);
+        this.createShadowRoot().appendChild(element);
+    };
+
+    /*
+     userProfileProto.attributeChangedCallback=function(name,oldVal,newVal){
+     console.log(name);
+     console.log(newVal);
+     };
+     */
+
+    var userProfile = document.registerElement('user-profile', {
+        prototype: userProfileProto
+    });
+}
+
+function Model(){
+    var people=[
+        {
+            name: "john",
+            last_name:"morales",
+            email:"john@rockalabs.com"
+        },
+        {
+            name: "sergio",
+            last_name:"florez",
+            email:"sergio@rockalabs.com"
+        }
+    ];
+
+    for (i = 0; i < people.length; i++) {
+        var userPr = document.createElement('user-profile');
+        userPr.init(people[i].name,people[i].last_name,people[i].email);
+        document.body.appendChild(userPr);
+    }
+}
+
+window.onload=function(){
+    //template("John","Morales","john@rockalabs.com");
+    //template("Sergio","Florez","sergio@rockalabs.com");
+    var link = document.querySelector('link[rel="import"]');
+    link.addEventListener('load', function(e) {
+        var importedDoc = link.import;
+        console.log(importedDoc);
+        // importedDoc points to the document under component.html
+    });
+    // customElement();
+    //Model();
+    /*
+     var userPr = document.createElement('user-profile');
+     userPr.init("John","Morales","john@rockalabs.com");
+     document.body.appendChild(userPr);
+     */
+};
--- readme.md
+++ readme.md
@@ -1,2 +1,19 @@
 Web Components
 ======================
+
+This is a example of web components using the four Specs.
+
+**Custom Elements:** allows authors to define their own custom tags (spec).
+
+**HTML Imports:** a way to include and reuse HTML documents via other HTML documents (spec).
+
+**Shadow DOM:** provides encapsulation by hiding DOM subtrees under shadow roots (spec).
+
+**Template**: Model Rendering of templates which this are activated when component is rendered.
+
+
+**Note: 
+For Run example you must run on server, because HTML imports follow the same restrictions as XHR.
+Need to be fetched from http(s). Blink imposes security restrictions on what you can do with file://.**
+-Eric Bidelman- https://groups.google.com/forum/#!msg/polymer-dev/Iz3pc2UKsAY/aLSrAN76GYcJ
+
