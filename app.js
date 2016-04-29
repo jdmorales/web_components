fbf8b06479cfbd74cecc95d4bb98df1031ef8e32
jdmorales <jdmg44@hotmail.com>
2016-04-29 10:38:29 +0000

    step six->Load Model API

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-function template(Name,Last_Name,Email){
+function template(person){
     var link = document.querySelector('link[rel="import"]').import;
-    console.log();
 
     var template = link.querySelector('#profileTemplate').content;//document.querySelector('#profileTemplate').content;
 
-    template.querySelector('.name').textContent = Name;
-    template.querySelector('.last_name').textContent = Last_Name;
-    template.querySelector('.email').textContent = Email;
+    template.querySelector('.name').textContent = person.name;
+    template.querySelector('.cell').textContent= person.cell;
+    template.querySelector('.last_name').textContent = person.last_name;
+    template.querySelector('.email').textContent = person.email;
+    //var wrapperImg=template.querySelector('wrapper-img');
+    var img=template.querySelector('img');
+    //var htmlSpinner='<i class="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i>';
+    img.src= person.photo;
+    
+    img.onload=function(){
+      //var spinner=template.querySelector('i.fa');
+      //spinner.style.display="none";
+    }
+    
 
     var clone = document.importNode(template, true); //Creates a copy of a node (node_template,deeps)
 
@@ -35,7 +45,15 @@ function customElement(){
     });
 }
 
+
+function appendElement(person){
+    var userPr = document.createElement('user-profile');
+    userPr.init(person);
+    document.body.appendChild(userPr);
+}
+
 function Model(){
+    /*
     var people=[
         {
             name: "john",
@@ -48,19 +66,27 @@ function Model(){
             email:"sergio@rockalabs.com"
         }
     ];
+    */
     
-    $.ajax({
-      url: 'http://api.randomuser.me/?results=50',
-      dataType: 'json',
-      success: function(data){
-        console.log(data);
-      }
-    });
-
-    for (i = 0; i < people.length; i++) {
-        var userPr = document.createElement('user-profile');
-        userPr.init(people[i].name,people[i].last_name,people[i].email);
-        document.body.appendChild(userPr);
+    var numPeople=50;
+    
+    for (i = 0; i < numPeople; i++) {
+        $.ajax({
+              url: 'https://randomuser.me/api/?inc=email,phone,gender,name,picture,cell',
+              dataType: 'json',
+              success: function(data){
+                var result=data.results[0];
+                var person={
+                    name: result.name.first,
+                    last_name: result.name.last,
+                    email: result.email,
+                    cell:result.cell,
+                    photo:result.picture.large,
+                }
+                //console.log(person);
+                appendElement(person);
+              }
+        });
     }
 }
 
